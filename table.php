5690658f8d169850d24e2791cb458de3b810382c
Nick Kardakov <Nikita.Kardakov@gmail.com>
2015-05-14 10:56:23 +0100

    Index and table reload change

--- table.php
+++ table.php
@@ -1,5 +1,7 @@
 <?PHP
     
+    echo "<head><meta charset='UTF-8'></head>";
+    
     function readConfig() {
         $handle = fopen("config.txt", "r");
         
@@ -35,10 +37,40 @@
         
         if (($handle = fopen($url, "r")) !== FALSE) {
             while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
-                $spreadsheet_data[]=$data;
+                
+                $res = json_encode($data);
+                
+                $err = json_last_error();
+                
+                if ($err != 0) {
+                    echo $currentPage;
+                    echo 'Error: ';
+                    foreach ($data as $key => $val) {
+                        echo $key . " -> " . $val . " " . strlen(json_encode($val)) . "</br>";
+                    }
+                    echo "<br/><br/><br/>";
+                }
+                
+                $spreadsheet_data[]= $data;
             }
-
+            
             $res = json_encode($spreadsheet_data);
+
+            $err = json_last_error();
+            
+            $json_errors=array( -1 => 'An unknown error occured',
+                               JSON_ERROR_NONE => 'No error has occurred',
+                               JSON_ERROR_DEPTH => 'The maximum stack depth has been exceeded',
+                               JSON_ERROR_CTRL_CHAR => 'Control character error, possibly incorrectly encoded',
+                               JSON_ERROR_STATE_MISMATCH => 'Invalid or malformed JSON',
+                               JSON_ERROR_SYNTAX => 'Syntax error',
+                               JSON_ERROR_UTF8 => 'Malformed UTF-8 characters, possibly incorrectly encoded');
+            
+            if ($err != JSON_ERROR_NONE) {
+                echo $currentPage;
+                echo 'Error: ';
+                echo isset($json_errors[$err])?$json_errors[$err]:$json_errors[-1];
+            }
             
             $res = preg_replace_callback('/\\\\u([0-9a-f]{4})/i', function ($matches) {
                                          $sym = mb_convert_encoding(pack('H*', $matches[1]),'UTF-8','UTF-16');
@@ -60,7 +92,9 @@
     
     $output = "";
     
-    readPage(0, 12, function($page, $info){
+    
+    
+    readPage(0, $numberOfPages, function($page, $info){
              
          if ( $page != -1 ) {
              
@@ -68,11 +102,19 @@
              $header = $headers[$page];
              
              global $output;
-             $output = $output . "\"" . $header . "\":" . $info;
-             global $numberOfPages;
-             if ( $page != $numberOfPages - 1 ) {
-                $output = $output . ",";
+             
+             if ( strlen($info) > 0 ) {
+                $output = $output . "\"" . $header . "\":" . $info;
+             
+             //             echo "<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><b>" . $header . "</b><br/><br/>";
+             
+                global $numberOfPages;
+                if ( $page != $numberOfPages - 1 ) {
+                    $output = $output . ",";
+                }
              }
+             
+
              
          } else {
              
@@ -82,7 +124,7 @@
              $output = "{" . $output . "}";
              
              if ( strlen($output) > 500 ) {
-                 echo $output;
+//                 echo $output;
                  fwrite($fp, $output);
              }
              fclose($fp);
